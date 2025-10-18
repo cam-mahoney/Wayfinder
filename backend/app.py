87d59944685547a0b580ec33bb0c039f36ce7bdd
Bethany <bethany.aung@gmail.com>
2025-10-18 16:46:18 -0500

    Updated app.py with logic for storing user data

--- backend/app.py
+++ backend/app.py
@@ -22,8 +22,28 @@ def health():
 # ----------------------------
 # In-memory user storage
 # ----------------------------
-
+"""
+# USERS[user_id] = {
+#   "user_id": str,
+#   "first_name": str,
+#   "last_name": str,   
+#   "slider_responses": {question: int},
+# }
+"""
 USERS = {}
+
+def new_user(first_name:str, last_name:str):
+    uid = str(uuid4())
+    
+    user = {
+        "user_id": uid,
+        "first_name": first_name,
+        "last_name": last_name, 
+        "slider_responses": {} 
+    }
+    
+    USERS[uid] = user
+    return user 
 
 
 # ----------------------------
@@ -37,6 +57,8 @@ USERS = {}
 # ----------------------------
 # Routes
 # ----------------------------
+
+
 
 
 
